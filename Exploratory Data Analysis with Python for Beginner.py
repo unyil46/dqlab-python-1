833532d10a606994e95f9bc152f21351eb57311a
Unyil <reakjeasz@gmail.com>
2022-08-14 01:39:41 +0700

    Update Exploratory Data Analysis with Python for Beginner.py

--- Exploratory Data Analysis with Python for Beginner.py
+++ Exploratory Data Analysis with Python for Beginner.py
@@ -1,4 +1,5 @@
 #Exploratory Data Analysis with Python for Beginner
+#Tugas dari Andra
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,15 +10,15 @@ print(median_price)
 # Ubah freight_value menjadi shipping_cost dan cari shipping_cost 
 # termahal dari data penjualan tersebut menggunakan sort.
 order_df.rename(columns={"freight_value": "shipping_cost"}, inplace=True)
-sort_value = order_df.sort_values(by="price", ascending=0)
+sort_value = order_df.sort_values(by="shipping_cost", ascending=0)
 print(sort_value)
 # Untuk product_category_name, berapa  rata-rata weight produk tersebut 
 # dan standar deviasi mana yang terkecil dari weight tersebut, 
 mean_value = order_df["product_weight_gram"].groupby(order_df["product_category_name"]).mean()
 print(mean_value.sort_values())
-std_value = order_df["product_weight_gram"].groupby(order_df["product_category_name"]).min()
+std_value = order_df["product_weight_gram"].groupby(order_df["product_category_name"]).std()
 print(std_value.sort_values())
 # Buat histogram quantity penjualan dari dataset tersebutuntuk melihat persebaran quantity 
 # penjualan tersebut dengan bins = 5 dan figsize= (4,5)
-#order_df[["quantity"]].hist(figsize=(4, 5), bins=5)
-#plt.show()
+order_df[["quantity"]].hist(figsize=(4, 5), bins=5)
+plt.show()
