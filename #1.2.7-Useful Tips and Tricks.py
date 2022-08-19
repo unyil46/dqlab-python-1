9a346d19c64a1c1e4494e47bc214fd18da1ad3d0
Unyil <reakjeasz@gmail.com>
2022-08-20 00:34:03 +0700

    Create #1.2.7-Useful Tips and Tricks.py

---
+++ #1.2.7-Useful Tips and Tricks.py
@@ -0,0 +1,20 @@
+#Useful Tips and Tricks
+#https://academy.dqlab.id/main/livecode/160/296/1327?pr=0
+# Tuple
+print(">>> Tuple")
+tuple_menu = ('Gado-gado','Ayam Goreng','Rendang','Ketoprak')
+jumlah_menu = len(tuple_menu)
+print(jumlah_menu)
+# List
+print(">>> List")
+list_menu = ['Gado-gado','Ayam Goreng','Rendang','Ketoprak']
+jumlah_menu = len(list_menu)
+print(jumlah_menu)
+# Konversi tipe data
+print(">>> Konversi tipe data")
+list_buah = ['Apel', 'Apel', 'Jeruk', 'Markisa', 'Jeruk', 'Markisa', 'Apel']
+set_buah = set(list_buah)
+print(set_buah)
+list_buah = list(set_buah)
+list_buah.sort()
+print(list_buah)
