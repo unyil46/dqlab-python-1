6010c2ec295ee058f687d3fff568cf9aee9454c7
Unyil <reakjeasz@gmail.com>
2022-08-20 01:41:44 +0700

    Update #6-Data Visualization with Python Matplotlib for Beginner - Part 2.py

--- #6-Data Visualization with Python Matplotlib for Beginner - Part 2.py
+++ #6-Data Visualization with Python Matplotlib for Beginner - Part 2.py
@@ -69,3 +69,317 @@ plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.5), shadow=True, ncol=3, 
 plt.gcf().set_size_inches(10, 5) 
 plt.tight_layout() 
 plt.show() 
+
+#Membuat Line Chart GMV Breakdown by Top Provinces
+#https://academy.dqlab.id/main/livecode/165/318/1487?pr=0
+import datetime
+import pandas as pd
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+# Buat variabel untuk 5 propinsi dengan GMV tertinggi
+top_provinces = (dataset.groupby('province')['gmv']
+				 .sum() 
+				 .reset_index()
+				 .sort_values(by='gmv' ,ascending=False)
+				 .head(5)) 
+print(top_provinces) 
+
+# Buat satu kolom lagi di dataset dengan nama province_top
+dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
+
+# Plot multi-line chartnya 
+import matplotlib.pyplot as plt 
+dataset.groupby(['order_month', 'province_top' ])['gmv' ].sum().unstack().plot(marker='.', cmap='plasma') 
+plt.title('Monthly GMV Year 2019 - Breakdown by Province' ,loc='center' ,pad=30, 
+fontsize=20, color='blue') 
+plt.xlabel('Order Month', fontsize = 15) 
+plt.ylabel('Total Amount (in Billions)' ,fontsize = 15) 
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5) 
+plt.ylim(ymin=0) 
+labels, locations = plt.yticks() 
+plt.yticks(labels, (labels/1000000000).astype(int)) 
+plt.legend(loc='upper center', bbox_to_anchor=(1.1, 1), shadow=True, ncol=1)
+plt.gcf().set_size_inches(12, 5) 
+plt.tight_layout() 
+plt.show() 
+
+#Membuat Anotasi
+#https://academy.dqlab.id/main/livecode/165/318/1489?pr=0
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+top_provinces = (dataset.groupby('province')['gmv']
+                        .sum()
+                        .reset_index()
+                        .sort_values(by='gmv',ascending=False)
+                        .head(5))
+dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
+
+dataset.groupby(['order_month','province_top'])['gmv'].sum().unstack().plot(marker='.', cmap='plasma')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province',loc='center',pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)',fontsize = 15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='upper center', bbox_to_anchor=(1.1, 1), shadow=True, ncol=1)
+# Anotasi pertama 
+plt.annotate('GMV other meningkat pesat', xy=(5, 900000000),
+			 xytext=(4, 1700000000), weight='bold', color='red',
+			 arrowprops=dict(arrowstyle='fancy',
+							 connectionstyle='arc3',
+							 color='red')) 
+# Anotasi kedua
+plt.annotate('DKI Jakarta mendominasi', xy=(3, 3350000000),
+			 xytext=(0, 3700000000), weight='bold', color='red',
+			 arrowprops=dict(arrowstyle='->',
+							 connectionstyle='angle',
+							 color='red')) 
+plt.gcf().set_size_inches(12, 5)
+plt.tight_layout()
+plt.show()
+
+#Membuat Subset Data
+#https://academy.dqlab.id/main/livecode/165/319/1490?pr=0
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+print(dataset_dki_q4.head())
+
+#Membuat Pie Chart
+#https://academy.dqlab.id/main/livecode/165/319/1491?pr=0
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+gmv_per_city_dki_q4 = dataset_dki_q4.groupby('city')['gmv'].sum().reset_index()
+plt.figure(figsize=(6,6)) 
+plt.pie(gmv_per_city_dki_q4['gmv'], labels = gmv_per_city_dki_q4['city' ],autopct='%1.2f%%') 
+plt.title('GMV Contribution Per City - DKI Jakarta in Q4 2019' ,loc='center' ,pad=30, fontsize=15, color='blue') 
+plt.show() 
+
+#https://academy.dqlab.id/main/livecode/165/319/1492?pr=0
+#Membuat Bar Chart
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+plt.clf() 
+dataset_dki_q4.groupby('city')['gmv'].sum().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('GMV Per City - DKI Jakarta in Q4 2019',loc='center',pad=30, fontsize=15, color='blue') 
+plt.xlabel('City', fontsize = 15) 
+plt.ylabel('Total Amount (in Billions)',fontsize = 15) 
+plt.ylim(ymin=0) 
+labels, locations = plt.yticks() 
+plt.yticks(labels, (labels/1000000000).astype(int)) 
+plt.xticks(rotation=0) 
+plt.show()
+
+#Membuat Multi-Bar Chart
+#https://academy.dqlab.id/main/livecode/165/319/1493?pr=0
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+dataset_dki_q4.groupby(['city', 'order_month'])['gmv'].sum().unstack().plot(kind='bar') 
+plt.title('GMV Per City, Breakdown by Month\nDKI Jakarta in Q4 2019' ,loc='center',pad=30, fontsize=15, color='blue') 
+plt.xlabel('Province', fontsize = 12) 
+plt.ylabel('Total Amount (in Billions)' ,fontsize = 12) 
+plt.legend(bbox_to_anchor=(1,1), shadow=True, title='Month')
+plt.ylim(ymin=0) 
+labels, locations = plt.yticks() 
+plt.yticks(labels, (labels/1000000000).astype(int)) 
+plt.xticks(rotation=45) 
+plt.tight_layout() 
+plt.show()
+
+#Membuat Stacked Chart
+#https://academy.dqlab.id/main/livecode/165/319/1494?pr=0
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+dataset_dki_q4.groupby(['order_month', 'city'])[ 'gmv'].sum().sort_values(ascending=False).unstack().plot(kind='bar', stacked=True) 
+plt.title('GMV Per Month, Breakdown by City\nDKI Jakarta in Q4 2019' ,loc='center' ,pad=30, fontsize=15, color='blue') 
+plt.xlabel('Order Month', fontsize = 12) 
+plt.ylabel('Total Amount (in Billions)' ,fontsize = 12) 
+plt.legend(bbox_to_anchor=(1, 1), shadow=True, ncol=1,title='City') plt.ylim(ymin=0)
+labels, locations = plt.yticks() 
+plt.yticks(labels, (labels/1000000000).astype(int)) 
+plt.xticks(rotation=0) 
+plt.tight_layout() 
+plt.show()
+
+#Membuat Agregat Data Customer
+#https://academy.dqlab.id/main/livecode/165/320/1496?pr=0
+#https://codeshare.io/bvkm07
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+data_per_customer = (dataset_dki_q4.groupby('customer_id')
+					 .agg ({'order _id' : 'nunique', 
+							 'quantity':'sum', 
+							 'gmv' : 'sum'})
+					 .reset_index()
+					 .rename(columns={'order_id' :'orders'}))
+print(data_per_customer.sort_values(by='orders' ,ascending=False))
+
+#Membuat Histogram - Part 1
+#https://academy.dqlab.id/main/livecode/165/320/1497?pr=0
+#https://codeshare.io/1Y9ke3
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+data_per_customer = (dataset_dki_q4.groupby('customer_id')
+                                   .agg({'order_id':'nunique', 
+                                         'quantity': 'sum', 
+                                         'gmv':'sum'})
+                                   .reset_index()
+                                   .rename(columns={'order_id':'orders'}))
+
+plt.clf()
+# Histogram pertama
+plt.figure()
+plt.hist(data_per_customer['orders'])
+plt.show()
+# Histogram kedua
+plt.figure()
+plt.hist(data_per_customer['orders'], range=(1,5))
+plt.title('Distribution of Number of Orders per Customer\nDKI Jakarta in Q4 2019', fontsize=15, color='blue')
+plt.xlabel('Number of Orders', fontsize=12)
+plt.ylabel('Number of Customers', fontsize = 12)
+plt.show()
+
+#Membuat Histogram - Part 2
+#https://academy.dqlab.id/main/livecode/165/320/1498?pr=0
+#https://codeshare.io/Pdlq9E
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+data_per_customer = (dataset_dki_q4.groupby('customer_id')
+                                   .agg({'order_id':'nunique', 
+                                         'quantity': 'sum', 
+                                         'gmv':'sum'})
+                                   .reset_index()
+                                   .rename(columns={'order_id':'orders'}))
+
+plt.figure(figsize=(10,5)) 
+plt.hist(data_per_customer['quantity'], bins=100, range=(1,200), color='brown')
+plt.title('Distribution of Total Quantity per Customer\nDKI Jakarta in Q4 2019',fontsize=15, color='blue') 
+plt.xlabel('Quantity', fontsize = 12) 
+plt.ylabel('Number of Customers' ,fontsize = 12) 
+plt.xlim(xmin=0,xmax=200) 
+plt.show() 
+
+#Membuat Histogram - Part 3
+#https://academy.dqlab.id/main/livecode/165/320/1499?pr=0
+#https://codeshare.io/pqEV9X
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+data_per_customer = (dataset_dki_q4.groupby('customer_id')
+                                   .agg({'order_id':'nunique', 
+                                         'quantity': 'sum', 
+                                         'gmv':'sum'})
+                                   .reset_index()
+                                   .rename(columns={'order_id':'orders'}))
+
+plt.figure(figsize=(10,5)) 
+plt.hist(data_per_customer['gmv'], bins=100, range=(1,200000000), color='green')
+plt.title('Distribution of Total GMV per Customer\nDKI Jakarta in Q4 2019' ,fontsize=15, color='blue') 
+plt.xlabel('GMV (in Millions)', fontsize = 12) 
+plt.ylabel('Number of Customers' ,fontsize = 12) 
+plt.xlim(xmin=0,xmax=200000000) 
+labels, locations = plt.xticks() 
+plt.xticks(labels, (labels/1000000).astype(int)) 
+plt.show() 
+
+#Membuat Scatterplot
+#https://academy.dqlab.id/main/livecode/165/320/1502?pr=0
+#https://codeshare.io/Jbnqj6
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+data_per_customer = (dataset_dki_q4.groupby('customer_id')
+                                   .agg({'order_id':'nunique', 
+                                         'quantity': 'sum', 
+                                         'gmv':'sum'})
+                                   .reset_index()
+                                   .rename(columns={'order_id':'orders'}))
+
+plt.clf()
+# Scatterplot pertama
+plt.figure()
+plt.scatter(data_per_customer['quantity'], data_per_customer['gmv'])
+plt.show()
+# Scatterplot kedua: perbaikan scatterplot pertama
+plt.figure(figsize=(10,8))
+plt.scatter(data_per_customer['quantity'],data_per_customer['gmv'], marker='+', color='red')
+plt.title('Correlation of Quantity and GMV per Customer\nDKI Jakarta in Q4 2019', fontsize=15, color='blue')
+plt.xlabel('Quantity', fontsize = 12)
+plt.ylabel('GMV (in Millions)', fontsize=12)
+plt.xlim(xmin=0,xmax=300)
+plt.ylim(ymin=0,ymax=150000000)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000).astype(int))
+plt.show()
