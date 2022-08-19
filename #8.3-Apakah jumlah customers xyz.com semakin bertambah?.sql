57d20b8b8ee9f30c7b1d4f9bf227471235f02e3a
Unyil <reakjeasz@gmail.com>
2022-08-20 00:11:40 +0700

    Create #8.3-Apakah jumlah customers xyz.com semakin bertambah?.sql

---
+++ #8.3-Apakah jumlah customers xyz.com semakin bertambah?.sql
@@ -0,0 +1,21 @@
+#Apakah jumlah customers xyz.com semakin bertambah?
+#https://academy.dqlab.id/main/livecode/246/421/2106?pr=0
+
+#SELECT * FROM customer limit 5;
+SELECT
+  quarter,
+  COUNT(DISTINCT(customerID)) AS total_customer
+FROM
+  (
+    SELECT
+      customerID,
+      createDate,
+      QUARTER(createDate) as quarter
+    FROM
+      customer
+    WHERE
+      createDate >= '2004-01-01'
+      AND createDate <= '2004-06-30'
+  ) AS tabel_b
+GROUP BY
+  quarter
