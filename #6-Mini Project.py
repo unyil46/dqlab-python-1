b2ea096eac21b8362d0cde72911632a4b00ea0a5
Unyil <reakjeasz@gmail.com>
2022-08-20 21:28:54 +0700

    Update #6-Mini Project.py

--- #6-Mini Project.py
+++ #6-Mini Project.py
@@ -52,3 +52,119 @@ plt.annotate('Terjadi lonjakan', xy=(7, 310), xytext=(8, 300),
 plt.gcf().set_size_inches(10, 5)
 plt.tight_layout()
 plt.show()
+
+#Case 3: Kuantitas penjualan brand top 5 selama Desember 2019
+#https://academy.dqlab.id/main/livecode/165/321/1506?pr=
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
+                .sum()
+                .reset_index()
+                .sort_values(by='quantity',ascending=False)
+                .head(5))
+dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
+
+plt.clf()
+dataset_top5brand_dec.groupby('brand')['product_id'].nunique().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('Number of Sold Products per Brand, December 2019',loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('Brand', fontsize = 15)
+plt.ylabel('Number of Products',fontsize = 15)
+plt.ylim(ymin=0)
+plt.xticks(rotation=0)
+plt.show()
+
+#Case 4: Penjulan produk diatas 100 dan dibawah 100 selama Desember 2019
+#https://academy.dqlab.id/main/livecode/165/321/1507?pr=
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
+                .sum()
+                .reset_index()
+                .sort_values(by='quantity',ascending=False)
+                .head(5))
+dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
+
+# Membuat dataframe baru, untuk agregat jumlah quantity terjual per product
+dataset_top5brand_dec_per_product = dataset_top5brand_dec.groupby(['brand','product_id'])['quantity'].sum().reset_index()
+
+# Beri kolom baru untuk menandai product yang terjual >= 100 dan <100
+dataset_top5brand_dec_per_product['quantity_group'] = dataset_top5brand_dec_per_product['quantity'].apply(lambda x: '>= 100' if x >= 100 else '< 100')
+dataset_top5brand_dec_per_product.sort_values('quantity', ascending = False, inplace = True)
+
+# Membuat referensi pengurutan brand berdasarkan banyaknya semua product
+s_sort = dataset_top5brand_dec_per_product.groupby('brand')['product_id'].nunique().sort_values(ascending = False)
+
+# Plot stacked barchart
+dataset_top5brand_dec_per_product.groupby(['brand', 'quantity_group'])['product_id'].nunique().reindex(index = s_sort.index, level = 'brand').unstack().plot(kind = 'bar', stacked = True)
+plt.title('Number of Sold Products per Brand, December 2019', loc = 'center', pad = 30, fontsize = 15, color = 'blue')
+plt.xlabel('Brand', fontsize = 15)
+plt.ylabel('Number of Products',fontsize = 15)
+plt.ylim(ymin = 0)
+plt.xticks(rotation = 0)
+plt.show()
+
+
+#Case 5: Murah atau mahalkah harga produk brand top 5
+#https://academy.dqlab.id/main/livecode/165/321/1508?pr=
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
+                .sum()
+                .reset_index()
+                .sort_values(by='quantity',ascending=False)
+                .head(5))
+dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
+
+plt.figure(figsize=(10, 5))
+plt.hist(dataset_top5brand_dec.groupby('product_id')['item_price'].median(), bins=10, stacked=True, range=(1,2000000), color='green')
+plt.title('Distribution of Price Median per Product\nTop 5 Brands in Dec 2019',fontsize=15, color='blue')
+plt.xlabel('Price Median', fontsize = 12)
+plt.ylabel('Number of Products', fontsize = 12)
+plt.xlim(xmin=0, xmax=2000000)
+plt.show()
+
+#Case 6a: Korelasi quantity vs GMV
+#https://academy.dqlab.id/main/livecode/165/321/1509?pr=
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
+                .sum()
+                .reset_index()
+                .sort_values(by='quantity',ascending=False)
+                .head(5))
+dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
+
+#agregat per product
+data_per_product_top5brand_dec = dataset_top5brand_dec.groupby('product_id').agg({'quantity': 'sum','gmv':'sum','item_price':'median'}).reset_index()
+
+#scatter plot
+plt.figure(figsize = (10, 8))      
+plt.scatter(data_per_product_top5brand_dec['quantity'],data_per_product_top5brand_dec['gmv'], marker = '+', color = 'red')
+plt.title('Correlation of Quantity and GMV per Product\nTop 5 Brands in December 2019', fontsize = 15, color = 'blue')
+plt.xlabel('Quantity', fontsize = 12)
+plt.ylabel('GMV (in Millions)', fontsize = 12)
+plt.xlim(xmin = 0, xmax = 300)
+plt.ylim(ymin = 0, ymax = 200000000)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000).astype(int))
+plt.show()
